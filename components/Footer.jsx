0a61c45c473dca5f61221c17a86467e917c68823
Imran <Imran.ice.nstu12@gmail.com>
2022-05-12 15:20:47 +0600

    Navbar,layout and footer done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiOutlineTwitter,
+} from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <p>2022 Ajiss Shop, All rights reserved.</p>
+      <p className="icons">
+        <AiFillFacebook />
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
